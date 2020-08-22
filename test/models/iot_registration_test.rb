eba25c697af79661711d75810c9082ff0a48b203
ashutosh <ashutoshranjansingh458@gmail.com>
2020-08-22 18:07:25 +0530

    This is the ninth commit

---
+++ test/models/iot_registration_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class IotRegistrationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/userregistration_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserregistrationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
